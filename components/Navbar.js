2501f855eb2945d53a6d77d2ffe87b1a5c5fe56c
Haley Fogelson <fogelsonhaley@gmail.com>
2021-03-17 11:38:53 -0400

    fixed the styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,22 +6,32 @@ import styles from "../styles/Navbar.module.scss"
 
 const Navbar = () => {
   return (
-    <div className={styles.Navbar}>
-      <div className={styles.flexWrapperOne}>
+    <div className="Navbar"
+    //styles={styles.Navbar}
+    >
+      
+      <nav>
+      <div style={{ width: "100%", flexDirection: "column" }}>
+      <a href="/">
         <img
           alt=""
-          className={styles.generateLogoText2}
+          styles={styles.generateLogoText2}
           src="https://static.overlay-tech.com/assets/f1973cfe-3358-4931-9e45-91fff14f0025.png"
         />
-        <p className={styles.ourHistory}>Our History</p>
-        <p className={styles.productDevelopment}>
-          Product Development
-        </p>
-        <p className={styles.impact}>Impact</p>
-        <p className={styles.members}>Members</p>
-        <p className={styles.clients}>Clients</p>
-      </div>
-      <div className={styles.line1} />
+        </a>
+        </div>
+        
+        <a className="ourHistory" styles={styles.ourHistory} href="/our-history">Our History </a>
+        <a className="productDevelopment" styles={styles.productDevelopment} href="/product-development">
+          Product Development 
+        </a>
+        <a className="Impact" styles={styles.impact} href="/impact">Impact </a>
+        <a styles={styles.members} href="/members">Members</a>
+        <a styles={styles.clients} href="/clients">Clients</a>
+       
+        </nav>
+      
+      <div styles={styles.line1} />
     </div>
   );
 };
